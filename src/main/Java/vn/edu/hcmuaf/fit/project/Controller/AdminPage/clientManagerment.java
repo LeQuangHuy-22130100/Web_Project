6596c93087aaaa85fd311ff37a0cb6dfe6a3b8e2
LeQuangHuy-22130100 <22130100@st.hcmuaf.edu.vn>
2025-12-27 20:40:19 +0700

    Revert "giua ky project 16/11/2025"
    This reverts commit 0b0237e992f659ea6887a96f7ebe8f8e40939e04.

--- src/main/Java/vn/edu/hcmuaf/fit/project/Controller/AdminPage/clientManagerment.java
+++
@@ -1,4 +0,0 @@
-package vn.edu.hcmuaf.fit.project.Controller.AdminPage;
-
-public class clientManagerment {
-}
--- src/main/webapp/JSPWeb/js/scoller_header.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('.header_home_menu');
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-    }
-);
