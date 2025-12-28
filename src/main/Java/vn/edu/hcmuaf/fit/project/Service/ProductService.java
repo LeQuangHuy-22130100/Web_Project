68a66879886d8571f959a4490560a7ed6619df19
LeQuangHuy-22130100 <22130100@st.hcmuaf.edu.vn>
2025-12-28 11:20:14 +0700

    update DAO - Service - Controller

---
+++ src/main/Java/vn/edu/hcmuaf/fit/project/Service/ProductService.java
@@ -0,0 +1,55 @@
+package vn.edu.hcmuaf.fit.project.Service;
+import vn.edu.hcmuaf.fit.project.DAO.dao.productDAO;
+import vn.edu.hcmuaf.fit.project.DAO.model.Product;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ProductService {
+    productDAO productDao= new productDAO();
+
+    public List<Product> getAll() throws SQLException, ClassNotFoundException {
+        return productDao.getAll();
+    }
+
+    public Product getProductDetail(String id) throws SQLException, ClassNotFoundException {
+        return productDao.getByID(id);
+    }
+
+    public List<Product> getProductbyCateID(String cateID) throws SQLException, ClassNotFoundException {
+        return productDao.getListProductByCategoryID(cateID);
+    }
+
+    public List<Product> listSizeRange(String SizeRange) throws SQLException, ClassNotFoundException {
+        return productDao.getListProductBySize(SizeRange);
+    }
+
+    public List<Product> getProductByPriceRange(String min, String max) throws SQLException, ClassNotFoundException{
+        return productDao.getListProductByPriceRange(min, max);
+    }
+
+    public Product findById(String id) throws SQLException, ClassNotFoundException {
+        return productDao.getByID(id);
+    }
+
+
+    // phan trang Product
+ 
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+//        ProductService productService = new ProductService();
+//        String sizeRange = "Dưới 2,5m";
+////        List<Product> list = productService.getProductbyCateID("1");
+//        List<Product> list = productService.listSizeRange(sizeRange);
+//        System.out.println(list);
+//        System.out.println();
+//
+//        ProductService productService = new ProductService();
+//        String min = "12000";
+//        String max = "trở lên";
+//        List<Product> getProductByPriceRange = productService.PageProducts(1);
+//        for (Product PriceRangeList : getProductByPriceRange) {
+//            System.out.println(PriceRangeList);
+//        }
+    }
+}
---
+++ src/main/Java/vn/edu/hcmuaf/fit/project/Service/UserService.java
@@ -0,0 +1,41 @@
+package vn.edu.hcmuaf.fit.project.Service;
+
+import vn.edu.hcmuaf.fit.project.DAO.dao.userDAO;
+import vn.edu.hcmuaf.fit.project.DAO.model.User;
+//import vn.edu.hcmuaf.fit.project.OTP.OTPGenerator;
+
+import java.sql.SQLException;
+
+public class UserService {
+    static userDAO userDao = new userDAO();
+
+    public User login(String username, String password) throws SQLException, ClassNotFoundException {
+        return userDao.login(username,password);
+    }
+
+    public void Signin(String username, String password) throws SQLException, ClassNotFoundException {
+        userDao.signin(username,password);
+    }
+
+    public User checkUser(String username) throws SQLException, ClassNotFoundException {
+        return userDao.checkUser(username);
+    }
+
+    public User checkPower(String isAdmin) throws SQLException, ClassNotFoundException {
+        return userDao.checkPower(isAdmin);
+    }
+
+
+
+//    public String generateAndSendOTP(String email) throws SQLException, ClassNotFoundException {
+//        User user = userDAO.findByEmail(email);
+//        if (user == null) {
+//            return null;
+//        }
+//
+//        String otp = OTPGenerator.generateOTP();
+//        emailService.sendOTP(email, otp);
+//        return otp;
+//    }
+
+}
