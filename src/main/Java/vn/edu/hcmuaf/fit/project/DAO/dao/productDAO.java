83f45c9f23336a19db1d9afcba0d57fe4d662ba9
LeQuangHuy-22130100 <22130100@st.hcmuaf.edu.vn>
2025-12-30 21:12:20 +0700

    Pagination  Dao - Service - Controller

--- src/main/Java/vn/edu/hcmuaf/fit/project/DAO/dao/productDAO.java
+++ src/main/Java/vn/edu/hcmuaf/fit/project/DAO/dao/productDAO.java
@@ -31,13 +31,12 @@ public class productDAO {
     public List<Product> getAll() throws SQLException, ClassNotFoundException {
         // Kết nối với database thông qua DBConnect
         rs = stmt.executeQuery(
-                            "SELECT p.*, \n" +
-                                    "       c.Name AS category_name, \n" +
-                                    "       s.Size AS size_name\n" +
-                                    "FROM product p\n" +
-                                    "JOIN categories c ON p.CategoryID = c.CategoryID\n" +
-                                    "JOIN sizes s ON p.SizeID = s.SizeID;"
-        );
+                "SELECT p.*, \n" +
+                        "       c.Name AS category_name, \n" +
+                        "       s.Size AS size_name\n" +
+                        "FROM product p\n" +
+                        "JOIN categories c ON p.CategoryID = c.CategoryID\n" +
+                        "JOIN sizes s ON p.SizeID = s.SizeID;");
 
         // Duyệt qua từng dòng dữ liệu và thêm vào danh sách
         while (rs.next()) {
@@ -63,11 +62,10 @@ public class productDAO {
     }
 
     public Product getByID(String id) throws SQLException, ClassNotFoundException {
-        String query =
-                "SELECT p.*, \n" +
-                        "c.Name AS category_name, \n" +
-                        "s.Size AS size_name\n" +
-                        "FROM product p\n" +
+        String query = "SELECT p.*, \n" +
+                "c.Name AS category_name, \n" +
+                "s.Size AS size_name\n" +
+                "FROM product p\n" +
                 "JOIN categories c ON p.CategoryId = c.CategoryId " +
                 "JOIN sizes s ON p.SizeID = s.SizeID " +
                 "WHERE p.ProductID = ?";
@@ -75,9 +73,8 @@ public class productDAO {
         Product product = null;
         try (
                 Connection con = DBConnect.get().getConnection();
-                PreparedStatement pstmt = con.prepareStatement(query)
-        ) {
-//            pstmt.setInt(1, id);
+                PreparedStatement pstmt = con.prepareStatement(query)) {
+            // pstmt.setInt(1, id);
             pstmt.setString(1, id);
             try (ResultSet rs = pstmt.executeQuery()) {
                 if (rs.next()) {
@@ -102,19 +99,17 @@ public class productDAO {
 
     public List<Product> getListProductByCategoryID(String CategoryID) throws SQLException, ClassNotFoundException {
         List<Product> products = new ArrayList<>();
-        String query =
-                "SELECT p.*, \n" +
-                        "c.Name AS category_name, \n" +
-                        "s.Size AS size_name\n" +
-                        "FROM product p\n" +
-                        "JOIN categories c ON p.CategoryId = c.CategoryId " +
-                        "JOIN sizes s ON p.SizeID = s.SizeID " +
+        String query = "SELECT p.*, \n" +
+                "c.Name AS category_name, \n" +
+                "s.Size AS size_name\n" +
+                "FROM product p\n" +
+                "JOIN categories c ON p.CategoryId = c.CategoryId " +
+                "JOIN sizes s ON p.SizeID = s.SizeID " +
                 "WHERE c.CategoryID = ?";
 
         try (
                 Connection con = DBConnect.get().getConnection();
-                PreparedStatement pstmt = con.prepareStatement(query)
-        ) {
+                PreparedStatement pstmt = con.prepareStatement(query)) {
             pstmt.setString(1, CategoryID);
             try (ResultSet rs = pstmt.executeQuery()) {
                 while (rs.next()) {
@@ -141,16 +136,14 @@ public class productDAO {
 
     public List<Product> getListProductBySize(String Size) throws SQLException, ClassNotFoundException {
         List<Product> products = new ArrayList<>();
-        String query =
-                "SELECT product.*, categories.Name AS category_name, sizes.Size AS size_name " +
-                        "FROM product " +
-                        "JOIN categories ON product.CategoryID = categories.CategoryID " +
-                        "JOIN sizes ON product.SizeID = sizes.SizeID " +
-                        "WHERE sizes.Size = ?";
+        String query = "SELECT product.*, categories.Name AS category_name, sizes.Size AS size_name " +
+                "FROM product " +
+                "JOIN categories ON product.CategoryID = categories.CategoryID " +
+                "JOIN sizes ON product.SizeID = sizes.SizeID " +
+                "WHERE sizes.Size = ?";
         try (
                 Connection con = DBConnect.get().getConnection();
-                PreparedStatement pstmt = con.prepareStatement(query)
-        ) {
+                PreparedStatement pstmt = con.prepareStatement(query)) {
             pstmt.setString(1, Size);
             try (ResultSet rs = pstmt.executeQuery()) {
                 while (rs.next()) {
@@ -175,23 +168,22 @@ public class productDAO {
         return products;
     }
 
-    public List<Product> getListProductByPriceRange(String minPrice, String maxPrice) throws SQLException, ClassNotFoundException {
+    public List<Product> getListProductByPriceRange(String minPrice, String maxPrice)
+            throws SQLException, ClassNotFoundException {
         List<Product> products = new ArrayList<>();
         String query = null;
-        query =
-                "SELECT p.*, \n" +
-                        "c.Name AS category_name, \n" +
-                        "s.Size AS size_name\n" +
-                        "FROM product p\n" +
-                        "JOIN categories c ON p.CategoryId = c.CategoryId " +
-                        "JOIN sizes s ON p.SizeID = s.SizeID " +
-                        "JOIN price_range pr ON p.PriceID = pr.PriceID " +
-                        "WHERE pr.PriceMin >= ? AND (pr.PriceMax = 'trở lên' OR pr.PriceMax <= ?)";
+        query = "SELECT p.*, \n" +
+                "c.Name AS category_name, \n" +
+                "s.Size AS size_name\n" +
+                "FROM product p\n" +
+                "JOIN categories c ON p.CategoryId = c.CategoryId " +
+                "JOIN sizes s ON p.SizeID = s.SizeID " +
+                "JOIN price_range pr ON p.PriceID = pr.PriceID " +
+                "WHERE pr.PriceMin >= ? AND (pr.PriceMax = 'trở lên' OR pr.PriceMax <= ?)";
 
         try (
                 Connection con = DBConnect.get().getConnection();
-                PreparedStatement pstmt = con.prepareStatement(query)
-        ) {
+                PreparedStatement pstmt = con.prepareStatement(query)) {
             pstmt.setString(1, minPrice);
             if (!"trở lên".equalsIgnoreCase(maxPrice)) {
                 pstmt.setString(2, maxPrice);
@@ -220,17 +212,16 @@ public class productDAO {
         return products;
     }
 
-        public List<Product> findProductbyName(String name) throws SQLException, ClassNotFoundException {
+    public List<Product> findProductbyName(String name) throws SQLException, ClassNotFoundException {
         List<Product> products = new ArrayList<>();
         String query = "SELECT *, sizes.Size\n" +
-                        "FROM Product\n" +
-                        "JOIN sizes ON product.SizeID = sizes.SizeID\n" +
-                        "WHERE Name LIKE ? COLLATE utf8mb4_general_ci;";
+                "FROM Product\n" +
+                "JOIN sizes ON product.SizeID = sizes.SizeID\n" +
+                "WHERE Name LIKE ? COLLATE utf8mb4_general_ci;";
         try (
 
                 Connection con = DBConnect.get().getConnection();
-                PreparedStatement pstmt = con.prepareStatement(query)
-        ) {
+                PreparedStatement pstmt = con.prepareStatement(query)) {
             pstmt.setString(1, "%" + name + "%");
             try (ResultSet rs = pstmt.executeQuery()) {
                 while (rs.next()) {
@@ -254,5 +245,21 @@ public class productDAO {
         }
         return products;
     }
+
+    // phan trang
+    public int countProduct() throws SQLException, ClassNotFoundException {
+        String query = "SELECT COUNT(*) FROM product";
+        try (
+                Connection con = DBConnect.get().getConnection();
+                PreparedStatement pstmt = con.prepareStatement(query)) {
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    return rs.getInt(1);
+                }
+            }
+        }
+        return 0;
+    }
+
+
 }
-    
